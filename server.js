ee9b7086dc6b63514989b5b8d7ff5492b2300960
Johnny <johnlabedzki@gmail.com>
2023-10-09 14:41:22 -0700

    add routes

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-/* Your routes here */
+const catData = {
+  joseph: {
+    age: 4,
+    // TODO: fill
+  },
+};
+
+app.get("/cat-data", (req, res) => {});
+
+app.post("/email-subscribe", (req, res) => {});
+
+app.post("/contact", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Joseph The Cat API listening on port ${port}`);
