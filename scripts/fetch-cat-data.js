1f31e3408e7f474b92d90ba132ed4b9adc043297
Johnny <johnlabedzki@gmail.com>
2023-10-10 07:36:39 -0700

    finish script to fetch cat data and populate cat comparison table with results

--- scripts/fetch-cat-data.js
+++ scripts/fetch-cat-data.js
@@ -5,6 +5,37 @@ const fetchCatData = async () => {
   return response.data;
 };
 
-fetchCatData().then((data) => {
-  console.log(data);
-});
+(async () => {
+  const catData = await fetchCatData();
+  const tableBody = document
+    .getElementById("cat-comparison")
+    .querySelector("tbody");
+
+  catData.forEach((cat) => {
+    const tr = document.createElement("tr");
+
+    Object.entries(cat).forEach(([key, value]) => {
+      const td = document.createElement("td");
+
+      if (typeof value === "boolean") {
+        const content = value ? "&#x2714;" : "&#10060;";
+        td.innerHTML = content;
+        if (value) {
+          td.classList.add("check-mark");
+        }
+      }
+
+      if (typeof value === "string") {
+        td.innerText = value.charAt(0).toUpperCase() + value.slice(1);
+      }
+
+      if (typeof value === "number") {
+        td.innerText = key === "weight" ? `${value}lbs` : value;
+      }
+
+      tr.appendChild(td);
+    });
+
+    tableBody.appendChild(tr);
+  });
+})();
