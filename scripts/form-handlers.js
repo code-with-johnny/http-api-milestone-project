40c8b51e40b839c7cd873fe32b1c6701d523e030
Johnny <johnlabedzki@gmail.com>
2023-10-10 20:42:26 -0700

    implement remaining FE/server logic for form submissions

--- scripts/form-handlers.js
+++ scripts/form-handlers.js
@@ -3,21 +3,48 @@ import { API_URL } from "../constants.js";
 const subscribeForm = document.getElementById("subscribe-form");
 const contactForm = document.getElementById("contact-form");
 
-subscribeForm.addEventListener("submit", function (e) {
+subscribeForm.addEventListener("submit", async function (e) {
   e.preventDefault();
   const email = this.email.value;
-  alert(
-    `Thank you for subscribing! A welcome email has been sent to ${email}.`
-  );
+
+  try {
+    const response = await axios.post(`${API_URL}/email-subscribe`, {
+      email,
+    });
+
+    if (response.status === 200) {
+      alert(
+        `Thank you for subscribing! A welcome email has been sent to ${email}.`
+      );
+    }
+  } catch (err) {
+    alert(err.message);
+  }
+
   this.reset();
 });
 
-contactForm.addEventListener("submit", function (e) {
+contactForm.addEventListener("submit", async function (e) {
   e.preventDefault();
   const name = this.name.value;
   const email = this.email.value;
-  alert(
-    `Thank you for contacting me, ${name}! I will reply to ${email} as soon as I can.`
-  );
+  const message = this.message.value;
+
+  try {
+    const response = await axios.post(`${API_URL}/contact`, {
+      name,
+      email,
+      message,
+    });
+
+    if (response.status === 200) {
+      alert(
+        `Thank you for contacting me, ${name}! I will reply to ${email} as soon as I can.`
+      );
+    }
+  } catch (err) {
+    alert(err.message);
+  }
+
   this.reset();
 });
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-import express from "express";
 import cors from "cors";
-import catData from "./cat-data.json" assert { type: "json" };
+import express from "express";
+import fs from "fs";
+
+const catData = JSON.parse(fs.readFileSync("./cat-data.json", "utf8"));
 const app = express();
 const port = 8080;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/cat-data", (req, res) => {
+app.get("/cat-data", (_, res) => {
   return res.send(catData);
 });
 
-app.post("/email-subscribe", (req, res) => {});
+app.post("/email-subscribe", (req, res) => {
+  const { email } = req.body;
 
-app.post("/contact", (req, res) => {});
+  if (!email) {
+    return res.sendStatus(400);
+  }
+
+  return res.sendStatus(200);
+});
+
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.sendStatus(400);
+  }
+
+  return res.sendStatus(200);
+});
 
 app.listen(port, () => {
   console.log(`Joseph The Cat API listening on port ${port}`);
